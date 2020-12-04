7728350a36c68ee99b8c0d866fa3ec9804908f96
caselongo <caselongo@gmail.com>
2020-12-04 14:39:34 +0100

    x

--- googleerror.go
+++ googleerror.go
@@ -7,9 +7,12 @@ type GoogleError struct {
 		Code    int    `json:"code"`
 		Message string `json:"message"`
 		Errors  []struct {
-			Domain  string `json:"domain"`
-			Message string `json:"message"`
-			Reason  string `json:"reason"`
+			Domain       string `json:"domain"`
+			Message      string `json:"message"`
+			Reason       string `json:"reason"`
+			Location     string `json:"location"`
+			LocationType string `json:"locationType"`
 		} `json:"errors"`
+		Status string `json:"status"`
 	} `json:"error"`
 }
