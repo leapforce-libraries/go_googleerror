ab704ca3e821bbcda331a0c5b2b8a271b251e940
caselongo <caselongo@gmail.com>
2020-12-05 11:10:51 +0100

    x

---
+++ Client.go
@@ -0,0 +1,83 @@
+package google
+
+import (
+	"bytes"
+	"net/http"
+
+	bigquerytools "github.com/leapforce-libraries/go_bigquerytools"
+	errortools "github.com/leapforce-libraries/go_errortools"
+	oauth2 "github.com/leapforce-libraries/go_oauth2"
+)
+
+// Client stores googleClient configuration
+//
+type GoogleClient struct {
+	oAuth2 *oauth2.OAuth2
+}
+
+type GoogleClientConfig struct {
+	APIName      string
+	ClientID     string
+	ClientSecret string
+	Scope        string
+	BigQuery     *bigquerytools.BigQuery
+}
+
+const (
+	authURL         string = "https://accounts.google.com/o/oauth2/v2/auth"
+	tokenURL        string = "https://oauth2.googleapis.com/token"
+	tokenHTTPMethod string = http.MethodPost
+	redirectURL     string = "http://localhost:8080/oauth/redirect"
+)
+
+// methods
+//
+func NewGoogleClient(googleClientConfig GoogleClientConfig) *GoogleClient {
+	gc := GoogleClient{}
+	maxRetries := uint(3)
+	config := oauth2.OAuth2Config{
+		APIName:         googleClientConfig.APIName,
+		ClientID:        googleClientConfig.ClientID,
+		ClientSecret:    googleClientConfig.ClientSecret,
+		Scope:           googleClientConfig.Scope,
+		RedirectURL:     redirectURL,
+		AuthURL:         authURL,
+		TokenURL:        tokenURL,
+		TokenHTTPMethod: tokenHTTPMethod,
+		MaxRetries:      &maxRetries,
+	}
+	gc.oAuth2 = oauth2.NewOAuth(config, googleClientConfig.BigQuery)
+	return &gc
+}
+
+func (gc *GoogleClient) InitToken() *errortools.Error {
+	return gc.oAuth2.InitToken()
+}
+
+func (gc *GoogleClient) get(url string, model interface{}) (*http.Request, *http.Response, *errortools.Error) {
+	err := ErrorResponse{}
+	request, response, e := gc.oAuth2.Get(url, model, &err)
+	if e != nil {
+		if err.Error.Message != "" {
+			e.SetMessage(err.Error.Message)
+		}
+
+		return request, response, e
+	}
+
+	return request, response, nil
+}
+
+func (gc *GoogleClient) patch(url string, requestBody []byte, model interface{}) (*http.Request, *http.Response, *errortools.Error) {
+	err := ErrorResponse{}
+	request, response, e := gc.oAuth2.Patch(url, bytes.NewBuffer(requestBody), model, &err)
+	if e != nil {
+		if err.Error.Message != "" {
+			e.SetMessage(err.Error.Message)
+		}
+
+		return request, response, e
+	}
+
+	return request, response, nil
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/leapforce-libraries/go_google
 
 go 1.13
+
+require (
+	github.com/leapforce-libraries/go_bigquerytools v0.0.0-20201127131929-ee11d040f98f
+	github.com/leapforce-libraries/go_errortools v0.0.0-20201204210737-d975f1003e2b
+	github.com/leapforce-libraries/go_oauth2 v0.0.0-20201205100818-a1a9276a6c5c
+)
