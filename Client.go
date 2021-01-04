ee842c9e9966127e70009eff780bf89e0bc56f2e
caselongo <caselongo@gmail.com>
2021-01-04 11:35:36 +0100

    x

--- Client.go
+++ Client.go
@@ -101,7 +101,7 @@ func (gc *GoogleClient) Patch(url string, requestBody []byte, model interface{})
 
 func getToken(apiName string, clientID string, bq *BigQuery) (*oauth2.Token, *errortools.Error) {
 	fmt.Println("***getToken***")
-	sqlSelect := "SELECT TokenType, AccessToken, RefreshToken, Expiry, Scope"
+	sqlSelect := "TokenType, AccessToken, RefreshToken, Expiry, Scope"
 	sqlWhere := fmt.Sprintf("Api = '%s' AND ClientID = '%s'", apiName, clientID)
 
 	type TokenBQ struct {
