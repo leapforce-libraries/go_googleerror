7d6be991b5f98da57b5340cc4c2f78cc29378366
caselongo <caselongo@gmail.com>
2021-01-11 16:09:13 +0100

    x

---
+++ Service.go
@@ -0,0 +1,109 @@
+package google
+
+import (
+	"net/http"
+
+	errortools "github.com/leapforce-libraries/go_errortools"
+	oauth2 "github.com/leapforce-libraries/go_oauth2"
+)
+
+// Service stores GoogleService configuration
+//
+type Service struct {
+	oAuth2   *oauth2.OAuth2
+	bigQuery *BigQuery
+}
+
+type ServiceConfig struct {
+	APIName           string
+	ClientID          string
+	ClientSecret      string
+	Scope             string
+	NonDefaultHeaders *http.Header
+}
+
+const (
+	authURL           string = "https://accounts.google.com/o/oauth2/v2/auth"
+	tokenURL          string = "https://oauth2.googleapis.com/token"
+	tokenHTTPMethod   string = http.MethodPost
+	redirectURL       string = "http://localhost:8080/oauth/redirect"
+	tableRefreshToken string = "leapforce.oauth2"
+)
+
+// methods
+//
+func NewService(serviceConfig ServiceConfig, bigQuery *BigQuery) *Service {
+	getTokenFunction := func() (*oauth2.Token, *errortools.Error) {
+		return GetToken(serviceConfig.APIName, serviceConfig.ClientID, bigQuery)
+	}
+
+	saveTokenFunction := func(token *oauth2.Token) *errortools.Error {
+		return SaveToken(serviceConfig.APIName, serviceConfig.ClientID, token, bigQuery)
+	}
+
+	maxRetries := uint(3)
+	oauht2Config := oauth2.OAuth2Config{
+		ClientID:          serviceConfig.ClientID,
+		ClientSecret:      serviceConfig.ClientSecret,
+		Scope:             serviceConfig.Scope,
+		RedirectURL:       redirectURL,
+		AuthURL:           authURL,
+		TokenURL:          tokenURL,
+		TokenHTTPMethod:   tokenHTTPMethod,
+		GetTokenFunction:  &getTokenFunction,
+		SaveTokenFunction: &saveTokenFunction,
+		NonDefaultHeaders: serviceConfig.NonDefaultHeaders,
+		MaxRetries:        &maxRetries,
+	}
+	return &Service{oauth2.NewOAuth(oauht2Config), bigQuery}
+}
+
+func (service *Service) InitToken() *errortools.Error {
+	return service.oAuth2.InitToken()
+}
+
+func (service *Service) Get(url string, responseModel interface{}) (*http.Request, *http.Response, *errortools.Error) {
+	err := ErrorResponse{}
+	request, response, e := service.oAuth2.Get(url, responseModel, &err)
+	return request, response, service.captureError(e, &err)
+}
+
+func (service *Service) Post(url string, bodyModel interface{}, responseModel interface{}) (*http.Request, *http.Response, *errortools.Error) {
+	err := ErrorResponse{}
+	request, response, e := service.oAuth2.Post(url, bodyModel, responseModel, &err)
+	return request, response, service.captureError(e, &err)
+}
+
+func (service *Service) Put(url string, bodyModel interface{}, responseModel interface{}) (*http.Request, *http.Response, *errortools.Error) {
+	err := ErrorResponse{}
+	request, response, e := service.oAuth2.Put(url, bodyModel, responseModel, &err)
+	return request, response, service.captureError(e, &err)
+}
+
+func (service *Service) Patch(url string, bodyModel interface{}, responseModel interface{}) (*http.Request, *http.Response, *errortools.Error) {
+	err := ErrorResponse{}
+	request, response, e := service.oAuth2.Patch(url, bodyModel, responseModel, &err)
+	return request, response, service.captureError(e, &err)
+}
+
+func (service *Service) Delete(url string, bodyModel interface{}, responseModel interface{}) (*http.Request, *http.Response, *errortools.Error) {
+	err := ErrorResponse{}
+	request, response, e := service.oAuth2.Delete(url, bodyModel, responseModel, &err)
+	return request, response, service.captureError(e, &err)
+}
+
+func (service *Service) captureError(e *errortools.Error, err *ErrorResponse) *errortools.Error {
+	if e == nil || err == nil {
+		return nil
+	}
+
+	if err.Error.Message != "" {
+		e.SetMessage(err.Error.Message)
+	}
+
+	return e
+}
+
+func (service *Service) ValidateToken() (*oauth2.Token, *errortools.Error) {
+	return service.oAuth2.ValidateToken()
+}
--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module github.com/leapforce-libraries/go_google
 go 1.15
 
 require (
-	cloud.google.com/go v0.72.0
+	cloud.google.com/go v0.74.0
 	cloud.google.com/go/bigquery v1.14.0
 	cloud.google.com/go/storage v1.12.0
-	github.com/leapforce-libraries/go_errortools v0.0.0-20201204210737-d975f1003e2b
-	github.com/leapforce-libraries/go_oauth2 v0.0.0-20210107213047-c513be79aae2
+	github.com/leapforce-libraries/go_errortools v0.0.0-20210111150536-e5045146a74f
+	github.com/leapforce-libraries/go_oauth2 v0.0.0-20210111150620-b07890fcce1e
 	github.com/leapforce-libraries/go_types v0.0.0-20210104111853-9f631f6df966
 	google.golang.org/api v0.36.0
 )
