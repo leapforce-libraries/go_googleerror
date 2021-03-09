dccf7e3d6e0b0e1545c031bf4b75733d6cfb8493
caselongo <caselongo@gmail.com>
2021-03-09 12:59:19 +0100

    x

--- Token.go
+++ Token.go
@@ -4,32 +4,39 @@ import (
 	"fmt"
 	"time"
 
-	_bigquery "cloud.google.com/go/bigquery"
+	"cloud.google.com/go/bigquery"
 	errortools "github.com/leapforce-libraries/go_errortools"
-	bigquery "github.com/leapforce-libraries/go_google/bigquery"
+	go_bigquery "github.com/leapforce-libraries/go_google/bigquery"
 	oauth2 "github.com/leapforce-libraries/go_oauth2"
 )
 
 type Token struct {
-	AccessToken  _bigquery.NullString
-	Scope        _bigquery.NullString
-	TokenType    _bigquery.NullString
-	RefreshToken _bigquery.NullString
-	Expiry       _bigquery.NullTimestamp
+	AccessToken  bigquery.NullString
+	Scope        bigquery.NullString
+	TokenType    bigquery.NullString
+	RefreshToken bigquery.NullString
+	Expiry       bigquery.NullTimestamp
 }
 
-func GetToken(apiName string, clientID string, service *bigquery.Service) (*oauth2.Token, *errortools.Error) {
+func GetToken(apiName string, clientID string, service *go_bigquery.Service) (*oauth2.Token, *errortools.Error) {
 	sqlSelect := "TokenType, AccessToken, RefreshToken, Expiry, Scope"
 	sqlWhere := fmt.Sprintf("Api = '%s' AND ClientID = '%s'", apiName, clientID)
 
 	token := new(Token)
 
-	e := service.GetStruct("", TableRefreshToken, sqlSelect, sqlWhere, token)
+	selectConfig := go_bigquery.SelectConfig{
+		DatasetName:     "",
+		TableOrViewName: TableRefreshToken,
+		SQLSelect:       sqlSelect,
+		SQLWhere:        sqlWhere,
+	}
+
+	_, e := service.GetStruct(&selectConfig, token)
 	if e != nil {
 		return nil, e
 	}
 
-	expiry := bigquery.NullTimestampToTime(token.Expiry)
+	expiry := go_bigquery.NullTimestampToTime(token.Expiry)
 
 	if expiry != nil {
 		//convert to UTC
@@ -39,16 +46,16 @@ func GetToken(apiName string, clientID string, service *bigquery.Service) (*oaut
 	}
 
 	return &oauth2.Token{
-		bigquery.NullStringToString(token.AccessToken),
-		bigquery.NullStringToString(token.Scope),
-		bigquery.NullStringToString(token.TokenType),
+		go_bigquery.NullStringToString(token.AccessToken),
+		go_bigquery.NullStringToString(token.Scope),
+		go_bigquery.NullStringToString(token.TokenType),
 		nil,
-		bigquery.NullStringToString(token.RefreshToken),
+		go_bigquery.NullStringToString(token.RefreshToken),
 		expiry,
 	}, nil
 }
 
-func SaveToken(apiName string, clientID string, token *oauth2.Token, service *bigquery.Service) *errortools.Error {
+func SaveToken(apiName string, clientID string, token *oauth2.Token, service *go_bigquery.Service) *errortools.Error {
 	if token == nil {
 		return nil
 	}
--- go.mod
+++ go.mod
@@ -3,11 +3,11 @@ module github.com/leapforce-libraries/go_google
 go 1.15
 
 require (
-	cloud.google.com/go v0.77.0
-	cloud.google.com/go/bigquery v1.15.0
-	cloud.google.com/go/storage v1.13.0
-	github.com/leapforce-libraries/go_errortools v0.0.0-20210113165137-bcbf687ce842
-	github.com/leapforce-libraries/go_http v0.0.0-20210219130405-85d07ecd3d24
+	cloud.google.com/go v0.75.0
+	cloud.google.com/go/bigquery v1.16.0
+	cloud.google.com/go/storage v1.14.0
+	github.com/leapforce-libraries/go_errortools v0.0.0-20210305222651-2350bce9f587
+	github.com/leapforce-libraries/go_http v0.0.0-20210308233523-938b6b2f9845
 	github.com/leapforce-libraries/go_oauth2 v0.0.0-20210219145500-dbb42a1d5772
 	github.com/leapforce-libraries/go_types v0.0.0-20210104111853-9f631f6df966
 	google.golang.org/api v0.40.0
