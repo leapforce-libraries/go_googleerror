6e63485386f984683b0789a2153e9766786312fd
caselongo <caselongo@gmail.com>
2022-01-31 11:47:44 +0100

    x

--- credentials/CredentialsJSON.go
+++ credentials/CredentialsJSON.go
@@ -6,7 +6,7 @@ type CredentialsJSON struct {
 	PrivateKeyID            string `json:"private_key_id"`
 	PrivateKey              string `json:"private_key"`
 	ClientEmail             string `json:"client_email"`
-	ClienID                 string `json:"client_id"`
+	ClientID                string `json:"client_id"`
 	AuthURI                 string `json:"auth_uri"`
 	TokenURI                string `json:"token_uri"`
 	AuthProviderX509CertURL string `json:"auth_provider_x509_cert_url"`
